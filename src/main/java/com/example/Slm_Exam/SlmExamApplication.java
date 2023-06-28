eb7b9afee9755cd5def873b1f807426e57ab6e3b
Jelena Kocic <wi22b087@technikum-wien.at>
2023-06-28 17:48:52 +0200

    Initial commit

---
+++ src/main/java/com/example/Slm_Exam/SlmExamApplication.java
@@ -0,0 +1,13 @@
+package com.example.Slm_Exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlmExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlmExamApplication.class, args);
+	}
+
+}
